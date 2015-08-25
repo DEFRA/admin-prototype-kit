082d6457da2d6600871e07d27b4bdc810923bc67
Cruikshanks <alan.cruikshanks@gmail.com>
2015-08-26 00:41:31 +0100

    Add dymaic routing example and update controller to handle pages in folders

--- app/controllers/proto_pages_controller.rb
+++ app/controllers/proto_pages_controller.rb
@@ -10,35 +10,42 @@ class ProtoPagesController < ApplicationController
   end
 
   def update
-    method_name = :"after_#{params['id']}"
+    # current_page always starts with pages/ however we're not interested in
+    # that bit.
+    @page = current_page.gsub('pages/', '')
+
+    # Check to see if we have any after_ hooks related to the page which is
+    # posting
+    method_name = :"#{convert_to_method_name('after', params['id'])}"
     send(method_name, params) if respond_to?(method_name, true)
 
-    # The pattern is that if a custom handler has been added for POST on the
-    # page it should set where to redirect to in the params.
+    # The pattern is that if an after_ hook has been added for page it should
+    # set where to redirect to in the params.
     redirect_to params[:redirect_to] and return if params[:redirect_to]
 
     # Else the default behaviour is to redirect back to the page which submitted
     # the request
-    redirect_to "/#{current_page}" and return if page_exists?
+    redirect_to "/#{current_page}" and return if page_exists?(@page)
 
     # As a fall back if the page name is unrecognised e.g. someone called
     # proto_pages_path('blah') and there is no pages/blah.html.erb we redirect
     # to the unknown page. This should hopefully help designers correct
     # their mistake.
-    @unknown_page = File.basename(current_page)
     render proto_pages_path('unknown')
   end
 
   private
 
   def load_page
-    method_name = :"before_#{File.basename(current_page)}"
+    method_name = :"#{convert_to_method_name('before', current_page)}"
     send(method_name) if respond_to?(method_name, true)
   end
 
-  def page_exists?
-    page_name = File.basename(current_page)
-    HighVoltage.page_ids.include?(page_name)
+  def convert_to_method_name(prefix, page)
+    "#{prefix}_#{page.gsub('pages/', '').tr('/', '_').downcase}"
   end
 
+  def page_exists?(page)
+    HighVoltage.page_ids.include?(page)
+  end
 end
