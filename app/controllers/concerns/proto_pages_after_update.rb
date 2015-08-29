485e4ed2315e371443988fbcf8db46b8377a51da
Cruikshanks <alan.cruikshanks@gmail.com>
2015-08-29 14:02:23 +0100

    Add comments explaining what these modules are for

--- app/controllers/concerns/proto_pages_after_update.rb
+++ app/controllers/concerns/proto_pages_after_update.rb
@@ -1,3 +1,8 @@
+# Used to hold 'hooks' which are called after a page is submitted. Each hook
+# must be defined as after_[name of page] for the hook to be called after the
+# page has been submitted. The intention is to provide an opportunity to use
+# some logic to prepare the system before the page is shown e.g. grabbing some
+# data.
 module ProtoPagesAfterUpdate
   def after_customer(params)
     params[:redirect_to] = proto_pages_path('index')
--- app/controllers/concerns/proto_pages_before_show.rb
+++ app/controllers/concerns/proto_pages_before_show.rb
@@ -1,3 +1,7 @@
+# Used to hold 'hooks' which are called before a page is shown. Each hook must
+# be defined as before_[name of page] for the hook to be called before the
+# page is shown. The intention is to provide an opportunity to use some logic
+# to prepare the system before the page is shown e.g. grabbing some data.
 module ProtoPagesBeforeShow
   def before_index
     @pages = HighVoltage.page_ids
